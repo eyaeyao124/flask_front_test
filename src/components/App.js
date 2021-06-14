332630ef9dcdf51506de6ca50f7e02bc0ce057e7
eyao <ykikiee124@gmail.com>
2021-06-14 19:32:55 +0900

    base 구축

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React, { Suspense } from 'react';
+import { Route, Switch } from "react-router-dom";
+
+import LandingPage from "./LandingPage/LandingPage"
+import NavBar from "./NavBar/NavBar"
+import Footer from "./Footer/Footer"
+
+
+function App() {
+  return (
+    <Suspense fallback={(<div>Loading...</div>)}>
+      <NavBar />
+      <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+        </Switch>
+      </div>
+      <Footer />
+    </Suspense>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Menu from "./Sections/Menu";
+
+const NavBar = () => {
+    return (
+        <div>
+            <div>메뉴 아이콘</div>
+            <Menu />
+            <div>장바구니</div>
+        </div>
+    )
+}
+
+export default NavBar
+

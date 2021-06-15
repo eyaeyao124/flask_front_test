4980baab06251aa93077ea8b20bf0c42eb2613f1
eyao <ykikiee124@gmail.com>
2021-06-16 03:07:16 +0900

    상품 상세 페이지 및 장바구니 페이지 전반적 기능구현

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 
 const Cart = () => {
+    const [CartList, setCartList] = useState([]);
+
+    useEffect(() => {
+        setCartList(JSON.parse(localStorage.getItem('cart')))
+    }, [])
+
     return (
         <div>
-            hihi
+            {
+                CartList && CartList.map((item, index)=>{
+                    return  <div key={index}>
+                            <div>{item.rate}</div>
+                            <div>{item.title}</div>
+                            <div>{item.description}</div>
+                            <div>{item.price*item.count}</div>
+                            <div>{item.date}</div>
+                            </div>
+                })
+            }
         </div>
     )
 }
--- src/components/Detail/Detail.js
+++ src/components/Detail/Detail.js
@@ -1,9 +1,82 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
+import { useHistory } from "react-router-dom";
+import { Button } from 'antd';
+import { MinusOutlined, PlusOutlined } from '@ant-design/icons';
 
-const Detail = () => {
+const Detail = ({match}) => {
+    const history = useHistory();
+    const lists = require(`../../dummy/${match.params.category}/${match.params.category}.json`);
+    
+    const [Item,setItem] = useState([]);
+    const [Count,setCount] = useState(1);
+    const [TotalPrice,setTotalPrice] = useState(0);
+    const [BasicPrice,setBasicPrice] = useState(0);
+
+    const productIncrease = () => {
+        setCount(Count+1)
+        setTotalPrice(TotalPrice + BasicPrice)
+    }
+
+    const productDecrease = () => {
+        if(Count !== 1) {
+            setCount(Count-1)
+            setTotalPrice(TotalPrice - BasicPrice)
+        }
+    }
+
+    const addCart = () => {
+        if(localStorage.getItem('cart')) {
+            const localInfo = JSON.parse(localStorage.getItem('cart'));
+            const itemInfo = {...Item[0],...{"count":Count}};
+
+            const valueCompare = localInfo.findIndex(
+                item => item.id === Item[0].id && item.categoty ===  Item[0].categoty  
+            );
+
+            if(valueCompare !== -1){
+                alert('이미 추가된 상품입니다.')
+            }else{
+                const addInfo = [...localInfo,itemInfo]
+                localStorage.setItem('cart', JSON.stringify(addInfo));
+                history.push("/cart");
+            }
+            
+        }else{
+            const itemInfo = [{...Item[0],...{"count":Count}}];
+            localStorage.setItem('cart', JSON.stringify(itemInfo));
+        }
+    }
+    
+    useEffect(() => {
+        const productItem = lists.filter(item => item.id === Number(match.params.id))
+        setItem(productItem)
+        setBasicPrice(productItem[0].price)
+        setTotalPrice(productItem[0].price)
+    }, [])
+
     return (
         <div>
-            detail
+            {
+                Item && Item.map((item, index)=>{
+                    return  <div key={index}>
+                            <div>{item.rate}</div>
+                            <div>{item.title}</div>
+                            <div>{item.description}</div>
+                            <div>{item.price}</div>
+                            <div>{item.date}</div>
+                            </div>
+                })
+            }
+            <Button.Group>
+                <Button onClick={productDecrease} icon={<MinusOutlined />} />
+                <Button onClick={productIncrease} icon={<PlusOutlined />} />
+            </Button.Group>
+            <div>합계 <span>{Count}</span></div>
+            <div>금액 <span>{TotalPrice}</span></div>
+            <div>
+                <button onClick={addCart}>장바구니 담기</button>
+                <button>주문하기</button>
+            </div>
         </div>
     )
 }
